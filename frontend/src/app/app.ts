efd36c1ece476218789e8a92286a0318cd02effa
pitercoding <piterg.bio@gmail.com>
2026-01-09 18:14:48 +0100

    feat(frontend): add Angular Material theme and toolbar navigation

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterLink
+],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('Springular AI');
 }
